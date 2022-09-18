d688f99efdfe40c2f2b854f33f082ba2ac22aea8
hezixu <hexu@rochexp.com>
2022-09-18 22:30:59 +0800

    update defines, core

--- Shalicon/src/main/scala/common/Defines.scala
+++ Shalicon/src/main/scala/common/Defines.scala
@@ -9,60 +9,69 @@ object Defines {
   val BUBBLE = 0x00000013.U(WORD_LEN_WIDTH)  // [ADDI x0,x0,0] = BUBBLE
 
   // ALU INSTRUCTION RELATED
-  val EXE_FUN_LEN = 8.W
-  val ALU_U_LUI     =   1.U(EXE_FUN_LEN)
-  val ALU_U_AUIPC   =   2.U(EXE_FUN_LEN)
-  val ALU_J_JAL     =   3.U(EXE_FUN_LEN)
-  val ALU_I_JALR    =   4.U(EXE_FUN_LEN)
-  val ALU_B_BEQ     =   5.U(EXE_FUN_LEN)
-  val ALU_B_BNE     =   6.U(EXE_FUN_LEN)
-  val ALU_B_BLT     =   7.U(EXE_FUN_LEN)
-  val ALU_B_BGE     =   8.U(EXE_FUN_LEN)
-  val ALU_B_BLTU    =   9.U(EXE_FUN_LEN)
-  val ALU_B_BGEU    =  10.U(EXE_FUN_LEN)
-  val ALU_I_LB      =  11.U(EXE_FUN_LEN)
-  val ALU_I_LH      =  12.U(EXE_FUN_LEN)
-  val ALU_I_LW      =  13.U(EXE_FUN_LEN)
-  val ALU_I_LBU     =  14.U(EXE_FUN_LEN)
-  val ALU_I_LHU     =  15.U(EXE_FUN_LEN)
-  val ALU_S_SB      =  16.U(EXE_FUN_LEN)
-  val ALU_S_SH      =  17.U(EXE_FUN_LEN)
-  val ALU_S_SW      =  18.U(EXE_FUN_LEN)
-  val ALU_I_ADDI    =  19.U(EXE_FUN_LEN)
-  val ALU_I_SLTI    =  20.U(EXE_FUN_LEN)
-  val ALU_I_SLTIU   =  21.U(EXE_FUN_LEN)
-  val ALU_I_XORI    =  22.U(EXE_FUN_LEN)
-  val ALU_I_ORI     =  23.U(EXE_FUN_LEN)
-  val ALU_I_ANDI    =  24.U(EXE_FUN_LEN)
-  val ALU_I_SLLI    =  25.U(EXE_FUN_LEN)
-  val ALU_I_SRLT    =  26.U(EXE_FUN_LEN)
-  val ALU_I_SRAI    =  27.U(EXE_FUN_LEN)
-  val ALU_R_ADD     =  28.U(EXE_FUN_LEN)
-  val ALU_R_SUB     =  29.U(EXE_FUN_LEN)
-  val ALU_R_SLL     =  30.U(EXE_FUN_LEN)
-  val ALU_R_SLT     =  31.U(EXE_FUN_LEN)
-  val ALU_R_SLTU    =  32.U(EXE_FUN_LEN)
-  val ALU_R_XOR     =  33.U(EXE_FUN_LEN)
-  val ALU_R_SRL     =  34.U(EXE_FUN_LEN)
-  val ALU_R_SRA     =  35.U(EXE_FUN_LEN)
-  val ALU_R_OR      =  36.U(EXE_FUN_LEN)
-  val ALU_R_AND     =  37.U(EXE_FUN_LEN)
-  val ALU_I_FENCE   =  38.U(EXE_FUN_LEN)
-  val ALU_I_FENCE_I =  39.U(EXE_FUN_LEN)
+  val EXE_FUN_LEN = 5.W
+  val ALU_X         =   0.U(EXE_FUN_LEN)
+  val ALU_ADD       =   1.U(EXE_FUN_LEN)
+  val ALU_SUB       =   2.U(EXE_FUN_LEN)
+  val ALU_AND       =   3.U(EXE_FUN_LEN)
+  val ALU_OR        =   4.U(EXE_FUN_LEN)
+  val ALU_XOR       =   5.U(EXE_FUN_LEN)
+  val ALU_SLL       =   6.U(EXE_FUN_LEN)
+  val ALU_SRL       =   7.U(EXE_FUN_LEN)
+  val ALU_SRA       =   8.U(EXE_FUN_LEN)
+  val ALU_SLT       =   9.U(EXE_FUN_LEN)
+
 
   // ALU OP1 RELATED
-  val OP1_LEN    =  3.W
-  val OP1_RS     =  0.U(OP1_LEN)
-  val OP1_PC     =  1.U(OP1_LEN)
+  val OP1_LEN    =  2.W
+  val OP1_X      =  0.U(OP1_LEN)
+  val OP1_RS     =  1.U(OP1_LEN)
+  val OP1_PC     =  2.U(OP1_LEN)
+  val OP1_IM_Z   =  3.U(OP1_LEN)
 
   val OP2_LEN    =  3.W
-  val OP2_RS     =  0.U(OP2_LEN)
-  val OP2_IM_I   =  1.U(OP2_LEN)
-  val OP2_IM_J   =  2.U(OP2_LEN)
-  val OP2_IM_S   =  3.U(OP2_LEN)
-  val OP2_IM_U   =  4.U(OP2_LEN)
+  val OP2_X      =  0.U(OP2_LEN)
+  val OP2_RS     =  1.U(OP2_LEN)
+  val OP2_IM_I   =  2.U(OP2_LEN)
+  val OP2_IM_J   =  3.U(OP2_LEN)
+  val OP2_IM_S   =  4.U(OP2_LEN)
+  val OP2_IM_U   =  5.U(OP2_LEN)
 
-  val
+  val MEN_LEN    =  2.W
+  val MEN_X      =  0.U(MEN_LEN)
+  val MEN_S      =  1.U(MEN_LEN) // Instructions for scala quantities
+  val MEN_V      =  2.U(MEN_LEN) // Instructions for vectors
+
+  val REN_LEN    =  2.W
+  val REN_X      =  0.U(REN_LEN)
+  val REN_S      =  1.U(REN_LEN)
+  val REN_V      =  2.U(REN_LEN)
+
+  val WB_SEL_LEN =  3.W
+  val WB_X       =  0.U(WB_SEL_LEN)
+  val WB_ALU     =  1.U(WB_SEL_LEN)
+  val WB_MEM     =  2.U(WB_SEL_LEN)
+  val WB_PC      =  3.U(WB_SEL_LEN)
+  val WB_CSR     =  4.U(WB_SEL_LEN)
+  val WB_MEM_V   =  5.U(WB_SEL_LEN)
+  val WB_ALU_V   =  6.U(WB_SEL_LEN)
+  val WB_VL      =  7.U(WB_SEL_LEN)
+
+  val MW_LEN     =  3.W
+  val MW_X       =  0.U(MW_LEN)
+  val MW_W       =  1.U(MW_LEN)
+  val MW_H       =  2.U(MW_LEN)
+  val MW_B       =  3.U(MW_LEN)
+  val MW_HU      =  4.U(MW_LEN)
+  val MW_BU      =  5.U(MW_LEN)
+
+  val CSR_LEN = 3.W
+  val CSR_X      =  0.U(CSR_LEN)
+  val CSR_W      =  1.U(CSR_LEN)
+  val CSR_S      =  2.U(CSR_LEN)
+  val CSR_C      =  3.U(CSR_LEN)
+  val CSR_E      =  4.U(CSR_LEN)
+  val CSR_V      =  5.U(CSR_LEN)
 }
 
 
--- Shalicon/src/main/scala/fetch/Core.scala
+++ Shalicon/src/main/scala/fetch/Core.scala
@@ -38,15 +38,58 @@ class Core extends Module {
   val imm_z = inst(19, 15)
   val imm_z_uext = Cat(Fill(27, 0.U), imm_z)
 
-  val decoded_inst = ListLookup(inst, List(), Array(
-    U_LUI -> List()
-  )
-  )
+  val decoded_inst = ListLookup(inst, List(ALU_X, OP1_X, OP2_X, MEN_X, REN_X, WB_X, CSR_X), Array(
+    // SW和LW
+    S_SW      -> List(ALU_ADD,  OP1_RS, OP2_IM_S, MEN_S, REN_X, WB_X,   CSR_X),
+    I_LW      -> List(ALU_ADD,  OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_MEM, CSR_X),
+    // 加减
+    R_ADD     -> List(ALU_ADD,  OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    R_SUB     -> List(ALU_SUB,  OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    I_ADDI    -> List(ALU_ADD,  OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_ALU, CSR_X),
+    // 逻辑
+    R_AND     -> List(ALU_AND,  OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    I_ANDI    -> List(ALU_AND,  OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_ALU, CSR_X),
+    R_OR      -> List(ALU_OR,   OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    I_ORI     -> List(ALU_OR,   OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_ALU, CSR_X),
+    R_XOR     -> List(ALU_XOR,  OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    I_XORI    -> List(ALU_OR,   OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_ALU, CSR_X),
+    // 移位
+    R_SLL     -> List(ALU_SLL,  OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    I_SLLI    -> List(ALU_SLL,  OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_ALU, CSR_X),
+    R_SRL     -> List(ALU_SRL,  OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    I_SRLI    -> List(ALU_SRL,  OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_ALU, CSR_X),
+    R_SRA     -> List(ALU_SRA,  OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    I_SRAI    -> List(ALU_SRA,  OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_ALU, CSR_X),
+    // 比较
+    R_SLT     -> List(ALU_SLT,  OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    I_SLTI    -> List(ALU_SLT,  OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_ALU, CSR_X),
+    R_SLTU    -> List(ALU_SLT,  OP1_RS, OP2_RS,   MEN_X, REN_S, WB_ALU, CSR_X),
+    I_SLTIU   -> List(ALU_SLT,  OP1_RS, OP2_IM_I, MEN_X, REN_S, WB_ALU, CSR_X)
+  ))
+  val exe_fun::op1_sel::op2_sel::mem_wen::rf_wen::wb_sel::csr_cmd::Nil = decoded_inst
+  val op1_data = MuxCase(0.U(WORD_LEN_WIDTH), Seq(
+    (op1_sel === OP1_RS)   -> rs1_data,
+    (op1_sel === OP1_PC)   -> pc_reg,
+    (op1_sel === OP1_IM_Z) -> imm_z_uext
+  ))
+  val op2_data = MuxCase(0.U(WORD_LEN_WIDTH), Seq(
+    (op2_sel === OP2_RS)   -> rs2_data,
+    (op2_sel === OP2_IM_I) -> imm_i_sext,
+    (op2_sel === OP2_IM_S) -> imm_s_sext,
+    (op2_sel === OP2_IM_J) -> imm_j_sext,
+    (op2_sel === OP2_IM_U) -> imm_u_shifted
+  ))
 
   // Excute (EX) Stage
-  val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
-    (inst === I_LW) -> (rs1_data + imm_i_sext),
-    (inst === S_SW) -> (rs1_data + imm_s_sext)
+  val alu_out = MuxCase(0.U(WORD_LEN_WIDTH), Seq(
+    (exe_fun === ALU_ADD)     -> (op1_data + op2_data),
+    (exe_fun === ALU_SUB)     -> (op1_data - op2_data),
+    (exe_fun === ALU_AND)     -> (op1_data & op2_data),
+    (exe_fun === ALU_OR)      -> (op1_data | op2_data),
+    (exe_fun === ALU_XOR)     -> (op1_data ^ op2_data),
+    (exe_fun === ALU_SLL)     -> (op1_data << op2_data(4, 0)).asUInt,
+    (exe_fun === ALU_SRL)     -> (op1_data >> op2_data(4, 0)).asUInt,
+    (exe_fun === ALU_SRA)     -> (op1_data)
   ))
 
   // Memory Access Stage
