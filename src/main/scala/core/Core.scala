2a439c2d1c3fd40b9f846ab200861e260b6e01dc
hezixu <hexu@rochexp.com>
2023-01-31 16:04:35 +0800

    new module: fifo

--- src/main/scala/core/Core.scala
+++ src/main/scala/core/Core.scala
@@ -5,6 +5,7 @@ trait CoreConfig {
   val xlen = 64.W
   val datawidth = 64.W
   val instwidth = 32.W
+  val regaddrwidth = 5.W
   val addrwidth = 64.W
   val resetVector = 0x60000000L.U(addrwidth)
 }
